e90831a141326244e040e6dc7ed6817a6cf40af6
ronniyjoseph <ronniyjoseph@gmail.com>
2019-07-23 09:37:55 +0800

    A code that generates sky and residual visibilities from the same sky realisation in an attempt to better understand the gain errors and their covariance

---
+++ create_sky_and_residual_visibilities.py
@@ -0,0 +1,196 @@
+import os
+import numpy
+from matplotlib import pyplot
+
+
+from radiotelescope import RadioTelescope
+from skymodel import SkyRealisation
+from gain_variance_simulation import get_observations
+
+from analytic_covariance import dft_matrix
+from analytic_covariance import blackman_harris_taper
+from analytic_covariance import sky_covariance
+from analytic_covariance import beam_covariance
+from analytic_covariance import plot_PS
+
+from generaltools import from_eta_to_k_par
+
+
+
+
+import time
+
+def main():
+    output_path = "/data/rjoseph/Hybrid_Calibration/Tile_Pertubation/Simulation_Output/"
+    project_path = "sky_and_residual_simulation"
+    n_realisations = 10000
+    frequency_range= numpy.linspace(135,165, 100)*1e6
+    shape = ['linear', 400, 20, 'log']
+    load = False
+    create_signal = False
+    compute_covariance = False
+    serial = True
+    plot_covariance = True
+
+    telescope = RadioTelescope(load=load, shape =shape)
+    baseline_table = telescope.baseline_table
+
+    if not os.path.exists(output_path + project_path + "/"):
+        print("Creating Project folder at output destination!")
+        os.makedirs(output_path + project_path)
+
+    if create_signal:
+        create_model_and_residuals(baseline_table, frequency_range, n_realisations, output_path + project_path)
+
+    if compute_covariance:
+        if serial:
+            compute_frequency_frequency_covariance_serial(baseline_table, frequency_range, output_path + project_path,
+                                                          n_realisations)
+        else:
+            pass
+
+    if plot_covariance:
+        #gain_covariance_impact(baseline_table, frequency_range, output_path + project_path)
+        residual_PS_error(baseline_table, frequency_range, output_path + project_path)
+
+
+    return
+
+
+def residual_PS_error(baseline_table, frequency_range, path, plot = True):
+    u = numpy.logspace(0, numpy.log10(200), 100)
+    cal_variance = numpy.zeros((len(u), len(frequency_range)))
+    raw_variance = numpy.zeros((len(u), len(frequency_range)))
+
+    frequency_frequency_covariance = numpy.load(path + "/Simulated_Covariance/" +
+                                                f"/frequency_frequency_covariance_9999.npy")
+    gain_covariance = numpy.sum(frequency_frequency_covariance, axis=0) / baseline_table.number_of_baselines**5
+    window_function = blackman_harris_taper(frequency_range)
+    taper1, taper2 = numpy.meshgrid(window_function, window_function)
+
+    dftmatrix, eta = dft_matrix(frequency_range)
+
+
+    for i in range(len(u)):
+        residual_covariance = sky_covariance(u[i], 0, frequency_range) + beam_covariance(u[i], v=0, nu=frequency_range)
+        model_covariance = sky_covariance(u[i], 0, frequency_range, S_low=1, S_high = 5)
+
+        nu_cov = 2*gain_covariance*model_covariance + (1 + 2*gain_covariance)*residual_covariance
+
+        cal_variance[i, :] = PS(taper1, taper2, nu_cov, dftmatrix)
+        raw_variance[i, :] = PS(taper1, taper2, residual_covariance, dftmatrix)
+    if plot:
+        plot_PS(u, eta[:int(len(eta)/2)], frequency_range, cal_variance[:, :int(len(eta) / 2)], cosmological=True,
+                title="Total", save=False, save_name=path + "/residual_calibrated_PS.pdf")
+        plot_PS(u, eta[:int(len(eta)/2)], frequency_range, raw_variance[:, :int(len(eta) / 2)], cosmological=True,
+                title="Total", save=False, save_name=path + "/residual_calibrated_PS.pdf")
+        plot_PS(u, eta[:int(len(eta)/2)], frequency_range, cal_variance[:, :int(len(eta) / 2)] - raw_variance[:, :int(len(eta) / 2)], cosmological=True,
+                title="Total", save=False, save_name=path + "/residual_calibrated_PS.pdf")
+
+    return
+
+
+
+def PS(taper1, taper2, covariance, dft_matrix):
+
+    tapered_cov = covariance * taper1 * taper2
+    eta_cov = numpy.dot(numpy.dot(dft_matrix.conj().T, tapered_cov), dft_matrix)
+    variance = numpy.diag(numpy.real(eta_cov))
+    return variance
+
+
+def gain_covariance_impact(baseline_table, frequency_range, path):
+    frequency_frequency_covariance = numpy.load(path + "/Simulated_Covariance/" +
+                                                f"/frequency_frequency_covariance_9999.npy")
+
+    gain_covariance = numpy.sum(frequency_frequency_covariance, axis=0) / baseline_table.number_of_baselines/226
+    pyplot.imshow(numpy.abs(gain_covariance))
+    pyplot.colorbar()
+    pyplot.show()
+
+    window_function = blackman_harris_taper(frequency_range)
+    taper1, taper2 = numpy.meshgrid(window_function, window_function)
+
+    dftmatrix, eta = dft_matrix(frequency_range)
+    tapered_cov = gain_covariance * taper1 * taper2
+    eta_cov = numpy.dot(numpy.dot(dftmatrix.conj().T, tapered_cov), dftmatrix)
+    gain_variance = numpy.diag(numpy.real(eta_cov))
+
+    k_parallel = from_eta_to_k_par(eta, frequency_range)
+    pyplot.semilogx(k_parallel[:int(len(eta) / 2)], gain_variance[:int(len(eta) / 2)])
+    pyplot.show()
+
+    return
+
+
+
+def create_model_and_residuals(baseline_table, frequency_range, n_realisations, path ):
+    print("Creating Signal Realisations")
+    if not os.path.exists(path + "/" + "Simulated_Visibilities"):
+        print("Creating realisation folder in Project path")
+        os.makedirs(path + "/" + "Simulated_Visibilities")
+
+    for i in range(n_realisations):
+        print(f"Realisation {i}")
+        signal_time0 = time.perf_counter()
+        source_population = SkyRealisation(sky_type='random', flux_high=10, gamma2=4, seed=i)
+
+        residual_indices = numpy.where(source_population.fluxes < 1)
+        model_indices = numpy.where(source_population.fluxes >= 1)
+
+        residual_sky = source_population.select_sources(residual_indices)
+        modelled_sky = source_population.select_sources(model_indices)
+
+        model_signal = get_observations(modelled_sky, baseline_table, frequency_range, interpolation='numba')
+        residual_signal = get_observations(residual_sky, baseline_table, frequency_range, interpolation='numba')
+
+        numpy.save(path +  "/" + "Simulated_Visibilities/" + f"model_realisation_{i}", model_signal)
+        numpy.save(path + "/" + "Simulated_Visibilities/" + f"residual_realisation_{i}", residual_signal)
+        signal_time1 = time.perf_counter()
+
+        print(f"Realisation {i} Time = {signal_time1 - signal_time0} \n")
+    return
+
+
+def compute_frequency_frequency_covariance_single(path, n_realisations, frequency_range,  baseline_index):
+    noise_signal_ratios = numpy.zeros((len(frequency_range), n_realisations), dtype=complex)
+    for i in range(n_realisations):
+        model = numpy.load(path + f"model_realisation_{i}.npy")
+        residual = numpy.load(path + f"residual_realisation_{i}.npy")
+        noise_signal_ratios[:, i] = residual[baseline_index, :] / model[baseline_index, :]
+    frequency_covariance = numpy.cov(noise_signal_ratios)
+
+    return frequency_covariance
+
+
+def compute_frequency_frequency_covariance_serial(baseline_table, frequency_range, path, n_realisations):
+    if not os.path.exists(path + "/" + "Simulated_Covariance"):
+        print("Creating realisation folder in Project path")
+        os.makedirs(path + "/" + "Simulated_Covariance")
+
+    baseline_frequency_covariance = numpy.zeros((baseline_table.number_of_baselines, len(frequency_range),
+                                                 len(frequency_range)), dtype=complex)
+
+    for j in range(baseline_table.number_of_baselines):
+        noise_signal_ratio = numpy.zeros((len(frequency_range), n_realisations), dtype = complex)
+
+        if not j%100 and j != 0 or j == 1:
+            print(f"Estimated time to finish re-processing = {delta_t*(baseline_table.number_of_baselines - j)}")
+
+        t0 = time.perf_counter()
+        for i in range(n_realisations):
+            model_signal = numpy.load(path + f"model_realisation_{i}.npy")
+            residual_signal = numpy.load(path + f"residual_realisation_{i}.npy")
+            noise_signal_ratio[:, i] = residual_signal[j, :] / model_signal[j, :]
+
+        baseline_frequency_covariance[j, ...] = numpy.cov(noise_signal_ratio)
+        t1 = time.perf_counter()
+        delta_t = t1 - t0
+
+    numpy.save(path + f"frequency_frequency_covariance", baseline_frequency_covariance)
+
+    return baseline_frequency_covariance
+
+
+if __name__ == "__main__":
+    main()
