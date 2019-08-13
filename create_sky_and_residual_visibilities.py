b83798ab30e64cbdbee2d2861981485c507d43d8
ronniyjoseph <ronniyjoseph@gmail.com>
2019-08-13 13:28:05 +0800

    Moved Residual PS plotter to seperature code for future code referencing

--- create_sky_and_residual_visibilities.py
+++ create_sky_and_residual_visibilities.py
@@ -100,66 +100,6 @@ def main():
     return
 
 
-def residual_ps_error(baseline_table, frequency_range, residuals ='both', path="./", plot = True):
-    u = numpy.logspace(0, numpy.log10(200), 100)
-    cal_variance = numpy.zeros((len(u), len(frequency_range)))
-    raw_variance = numpy.zeros((len(u), len(frequency_range)))
-
-    model_variance = numpy.diag(sky_covariance(0,0, frequency_range, S_low = 1, S_high = 10))
-    model_normalisation = numpy.sqrt(numpy.outer(model_variance, model_variance))
-    gain_error_covariance = numpy.zeros((len(u), len(frequency_range), len(frequency_range)))
-
-    #Compute all residual to model ratios at different u scales
-    for u_index in range(len(u)):
-        if residuals == "sky":
-            residual_covariance = sky_covariance(u[u_index], 0, frequency_range)
-        elif residuals == "beam":
-            residual_covariance = beam_covariance(u[u_index], v=0, nu=frequency_range)
-        elif residuals == 'both':
-            residual_covariance = sky_covariance(u[u_index], 0, frequency_range) + \
-                                  beam_covariance(u[u_index], v=0, nu=frequency_range)
-        gain_error_covariance[u_index, :, :] = residual_covariance/model_normalisation
-
-    gain_averaged_covariance = numpy.sum(gain_error_covariance, axis=0) / (baseline_table.number_of_baselines**4)
-
-    window_function = blackman_harris_taper(frequency_range)
-    taper1, taper2 = numpy.meshgrid(window_function, window_function)
-    dftmatrix, eta = dft_matrix(frequency_range)
-
-    #Compute the gain corrected residuals at all u scales
-    for i in range(len(u)):
-        if residuals == "sky":
-            residual_covariance = sky_covariance(u[i], 0, frequency_range)
-        elif residuals == "beam":
-            residual_covariance = beam_covariance(u[i], v=0, nu=frequency_range)
-        elif residuals == 'both':
-            residual_covariance = sky_covariance(u[i], 0, frequency_range) + \
-                                  beam_covariance(u[i], v=0, nu=frequency_range)
-
-        model_covariance = sky_covariance(u[i], 0, frequency_range, S_low=1, S_high= 5)
-
-        nu_cov = 2*gain_averaged_covariance*model_covariance + (1 + 2*gain_averaged_covariance)*residual_covariance
-
-        cal_variance[i, :] = PS(taper1, taper2, nu_cov, dftmatrix)
-        raw_variance[i, :] = PS(taper1, taper2, residual_covariance, dftmatrix)
-
-    if plot:
-        plot_PS(u, eta[:int(len(eta)/2)], frequency_range, cal_variance[:, :int(len(eta) / 2)], cosmological=True,
-                title="Calibrated Residuals", save=True, save_name=path + "/residual_calibrated_PS.pdf")
-        plot_PS(u, eta[:int(len(eta)/2)], frequency_range, raw_variance[:, :int(len(eta) / 2)], cosmological=True,
-                title="Uncalibrated Residuals", save=True, save_name=path + "/residual_uncalibrated_PS.pdf")
-        plot_PS(u, eta[:int(len(eta)/2)], frequency_range, cal_variance[:, :int(len(eta) / 2)] - raw_variance[:, :int(len(eta) / 2)], cosmological=True,
-                title="Difference", save=True, save_name=path + "/residual_difference_PS.pdf")
-        plot_PS(u, eta[:int(len(eta)/2)], frequency_range,
-                (cal_variance[:, :int(len(eta) / 2)] - raw_variance[:, :int(len(eta) / 2)])/raw_variance[:, :int(len(eta) / 2)], cosmological=True,
-                title="Ratio", save=True, save_name=path + "/residual_ratio_PS.pdf", ratio=True)
-
-        pyplot.show()
-
-    return cal_variance
-
-
-
 def PS(taper1, taper2, covariance, dft_matrix):
 
     tapered_cov = covariance * taper1 * taper2
