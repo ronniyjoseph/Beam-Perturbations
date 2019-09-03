2fde69ceec35c2f0cc82afd7feaafbbb2740d06b
ronniyjoseph <ronniyjoseph@gmail.com>
2019-09-03 14:55:07 +0800

    fiducial powerspectrum added

--- Plot_Gain_Error_PS.py
+++ Plot_Gain_Error_PS.py
@@ -1,33 +1,57 @@
 import numpy
 import matplotlib
+matplotlib.use('Agg')
 from matplotlib import pyplot
 from matplotlib import colors
 
 from generaltools import from_eta_to_k_par
-
+from generaltools import colorbar
 from analytic_covariance import gain_error_covariance
 from analytic_covariance import blackman_harris_taper
 from analytic_covariance import compute_ps_variance
 from analytic_covariance import dft_matrix
+from analytic_covariance import compute_weights
 
-from Plot_Calibrated_Error_Comparison import plot_power_spectrum
-
+from radiotelescope import RadioTelescope
+from plottools import plot_power_spectrum
 
 def main(labelfontsize = 10, ticksize= 10, plot_name = "Gain_PS_Window.pdf"):
     plot_path = "../../Plots/Analytic_Covariance/"
     u_range = numpy.logspace(0, numpy.log10(500), 100)
-    frequency_range = numpy.linspace(135, 165, 101) * 1e6
+    frequency_range = numpy.linspace(135, 165, 251) * 1e6
+
+    mwa_position_path = "./Data/MWA_Compact_Coordinates.txt"
+    mwa_telescope = RadioTelescope(load=True, path=mwa_position_path)
+    weights = compute_weights(u_range, mwa_telescope.baseline_table.u_coordinates,
+                              mwa_telescope.baseline_table.v_coordinates)
 
     gain_error_sky = gain_error_covariance(u_range, frequency_range, residuals='sky')
     gain_error_both = gain_error_covariance(u_range, frequency_range, residuals='both')
+    gain_error_sky_MWA = gain_error_covariance(u_range, frequency_range, residuals='sky', weights = weights)
+    gain_error_both_MWA = gain_error_covariance(u_range, frequency_range, residuals='both', weights = weights)
 
+
+    # fig, axs = pyplot.subplots(1,2)
+    # a = axs[0].pcolor(frequency_range, frequency_range, gain_error_sky - gain_error_both)
+    # b = axs[1].pcolor(frequency_range, frequency_range, gain_error_both )
+    # colorbar(a)
+    # colorbar(b)
+    # pyplot.show()
+    #
     window_function = blackman_harris_taper(frequency_range)
     taper1, taper2 = numpy.meshgrid(window_function, window_function)
     dftmatrix, eta = dft_matrix(frequency_range)
     k_perp = from_eta_to_k_par(eta[:int(len(eta) / 2)], nu_observed=150e6)
     gain_error_ps = numpy.tile(compute_ps_variance(taper1, taper2, gain_error_both, dftmatrix), (len(u_range), 1))
 
-    figure, axes = pyplot.subplots(1, 2, figsize = (10, 5))
+    mwa_sky_window = numpy.zeros((len(u_range), len(frequency_range)))
+    mwa_both_window = mwa_sky_window.copy()
+
+    for i in range(len(u_range)):
+        mwa_sky_window[i, :] = compute_ps_variance(taper1, taper2, gain_error_sky_MWA[i, ...], dftmatrix)
+        mwa_both_window[i, :] = compute_ps_variance(taper1, taper2, gain_error_both_MWA[i, ...], dftmatrix)
+
+    figure, axes = pyplot.subplots(1, 1, figsize=(10, 5))
 
     axes[0].plot(k_perp, compute_ps_variance(taper1, taper2, gain_error_sky, dftmatrix)[:int(len(eta) / 2)], label = r"$\mathbf{C}_{g}(sky)$")
     axes[0].plot(k_perp, compute_ps_variance(taper1, taper2, gain_error_both, dftmatrix)[:int(len(eta) / 2)], label = r"$\mathbf{C}_{g}(sky + beam)$")
@@ -37,10 +61,18 @@ def main(labelfontsize = 10, ticksize= 10, plot_name = "Gain_PS_Window.pdf"):
     axes[0].set_yscale('log')
     axes[0].legend()
 
-    # ps_norm = colors.LogNorm(vmin=1e3, vmax = 1e15)
-    plot_power_spectrum(u_range, eta[:int(len(eta) / 2)], frequency_range, gain_error_ps[:, :int(len(eta) / 2)], axes=axes[1],
+    #norm = colors.SymLogNorm(linthresh= 1e2, linscale = 1.5, vmin = -1e15, vmax = 1e15)
+    norm = colors.LogNorm()
+
+    plot_power_spectrum(u_range, eta[:int(len(eta) / 2)], frequency_range, mwa_sky_window[:, :int(len(eta) / 2)], axes=axes[1],
                         axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
-                        xlabel_show=True, ylabel_show=True, z_label="Dimensionless", ratio=True)
+                        xlabel_show=True, ylabel_show=True, z_label="Dimensionless", ratio=True, norm = norm)
+    # plot_power_spectrum(u_range, eta[:int(len(eta) / 2)], frequency_range, mwa_both_window[:, :int(len(eta) / 2)], axes=axes[2],
+    #                     axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
+    #                     xlabel_show=True, ylabel_show=True, z_label="Dimensionless", ratio=True, norm = norm)
+    # plot_power_spectrum(u_range, eta[:int(len(eta) / 2)], frequency_range, gain_error_ps[:, :int(len(eta) / 2)], axes=axes[1],
+    #                     axes_label_font=labelfontsize, tickfontsize=ticksize, colorbar_show=True,
+    #                     xlabel_show=True, ylabel_show=True, z_label="Dimensionless", ratio=True)
 
     figure.tight_layout()
     figure.savefig(plot_path + plot_name)
