908015d5493fb3bf0256fd53d3050435cb43e06d
ronniyjoseph <ronniyjoseph@gmail.com>
2019-09-03 15:30:31 +0800

    added argparse option to set the right backend

--- Plot_Calibrated_Error_Comparison.py
+++ Plot_Calibrated_Error_Comparison.py
@@ -1,11 +1,10 @@
 import numpy
 import matplotlib
-matplotlib.use('Agg')
-from matplotlib import pyplot
 from matplotlib import colors
 from plottools import plot_power_spectrum
 from analytic_covariance import residual_ps_error
 from Plot_Fiducial_PS import fiducial_eor
+import argparse
 
 
 def main(labelfontsize = 10, ticksize= 10):
@@ -21,8 +20,6 @@ def main(labelfontsize = 10, ticksize= 10):
 
     fiducial_ps = fiducial_eor(u_range, eta)
 
-
-
     figure, axes = pyplot.subplots(1, 3, figsize = (15, 5))
 
     ps_norm = colors.LogNorm(vmin = 1e3, vmax = 1e15)
@@ -32,28 +29,17 @@ def main(labelfontsize = 10, ticksize= 10):
                         xlabel_show= True, colorbar_show=True)
 
     diff_norm = colors.SymLogNorm(linthresh= 1e2, linscale = 1.5, vmin = -1e15, vmax = 1e15)
-   # diff_norm = colors.LogNorm(vmin = 1e5, vmax = 1e12)
-
     plot_power_spectrum(u_range, eta, frequency_range, difference_cal,
                         axes=axes[1], axes_label_font= labelfontsize, tickfontsize = ticksize,
                         norm=diff_norm, colorbar_show=True, xlabel_show= True,
                         title=r"$\mathbf{C}_{r}$(sky + beam) - $\mathbf{C}_{r}$(sky) ", diff=True,
                         colormap='coolwarm')
 
-
     ratio_norm = colors.SymLogNorm(linthresh= 1e1, linscale = 1, vmin = -1e5, vmax = 1e5)
     plot_power_spectrum(u_range, eta, frequency_range, difference_cal/fiducial_ps,
                         axes=axes[2], axes_label_font= labelfontsize, tickfontsize = ticksize,
                         norm=ratio_norm, colorbar_show=True, xlabel_show= True,
                         title=r"$(\mathbf{C}_{r}$(sky + beam) - $\mathbf{C}_{r}$(sky))/EoR ", diff=True)
-
-
-
-    # ratio_norm = colors.LogNorm(1e-2, 1e2)
-    # # Plot ratios with uncalibrated
-    # plot_power_spectrum(u_range, eta, frequency_range, (sky_and_beam_cal - sky_only_cal)/sky_only_cal,
-    #          ratio= True, axes=axes[2], axes_label_font= labelfontsize, tickfontsize = ticksize,
-    #         xlabel_show= True, colorbar_show=True, norm =ratio_norm, title="Fraction of Fiducial EoR Power")
 
     figure.tight_layout()
     figure.savefig(plot_path + "Comparing_Sky_and_Beam_Errors_Post_Calibration.pdf")
@@ -62,8 +48,10 @@ def main(labelfontsize = 10, ticksize= 10):
     return
 
 
-
-
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Plot and compare the sky and beam modelling errors')
+    parser.add_argument('-ssh', type=bool, action='store_true', default=False, help='flag to use when remote plotting')
+    if parser.ssh:
+        matplotlib.use('Agg')
+    from matplotlib import pyplot
     main()
