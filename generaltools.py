4079871e9b9c7187786559122811692b1a297fe1
ronniyjoseph <ronniyjoseph@gmail.com>
2019-09-03 15:04:53 +0800

    Moved colorbar function to plottools and removed unused modules

--- generaltools.py
+++ generaltools.py
@@ -2,22 +2,9 @@ import numpy
 import powerbox
 from scipy import interpolate
 from scipy.constants import c
-from scipy.constants import parsec
 from scipy.constants import Boltzmann
-from mpl_toolkits.axes_grid1 import make_axes_locatable
 from radiotelescope import beam_width
-import pyfftw
-import sys
 import time
-
-
-def colorbar(mappable):
-    ax = mappable.axes
-    fig = ax.figure
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-
-    return fig.colorbar(mappable, cax=cax)
 
 
 def symlog_bounds(data):
