d47208741ff7975f7ff9cfcf931b8624a428830e
ronniyjoseph <ronniyjoseph@gmail.com>
2019-09-03 14:56:16 +0800

    Fixed up fiducial interpolator and now handles csv fiducial data

--- Plot_Fiducial_PS.py
+++ Plot_Fiducial_PS.py
@@ -1,58 +1,76 @@
 import numpy
 import matplotlib
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 from matplotlib import pyplot
 from matplotlib import colors
 from analytic_covariance import dft_matrix
 
-from scipy.interpolate import interp2d
+from scipy.interpolate import interp1d
 
-from Plot_Calibrated_Error_Comparison import plot_power_spectrum
+from plottools import plot_power_spectrum
 
 
 def main(labelfontsize = 10, ticksize= 10):
-    frequency_range = numpy.linspace(135, 165, 101) * 1e6
+    output_path = "../../Plots/Analytic_Covariance/"
+    path = "/home/ronniyjoseph/Sync/PhD/Projects/beam_perturbations/code/tile_beam_perturbations/Data/"
+    file = "redshift8.csv"
+
+    frequency_range = numpy.linspace(135, 165, 251) * 1e6
     u_range = numpy.logspace(0, numpy.log10(500), 100)
     dftmatrix, eta = dft_matrix(frequency_range)
+    eta = eta[:len(eta)//2]
+    u_fiducial, eta_fiducial, fiducial_eor = read_data(path+file)
+    norm = colors.LogNorm(vmin=1e2, vmax=1e5)
 
-    u_fiducial, eta_fiducial, fiducial_eor = read_data()
+    fiducial_figure, fiducial_axes = pyplot.subplots(1, 1, figsize=(5, 5))
+    interpolation = interpolate(u_range, eta, u_fiducial, eta_fiducial, fiducial_eor)
 
-    fiducial_figure, fiducial_axes = pyplot.subplots(1, 2, figsize=(10, 5))
-    norm = colors.LogNorm(vmin=0.5e4, vmax=1.3e6)
-    interpolation = interp2d(u_fiducial, eta_fiducial, fiducial_eor)
-
-    plot_power_spectrum(u_range, eta_fiducial, frequency_range, fiducial_eor,
-                        ratio=True, axes=fiducial_axes[0], axes_label_font=labelfontsize, tickfontsize=ticksize,
+    plot_power_spectrum(u_range, eta, frequency_range, interpolation,
+                        ratio=True, axes=fiducial_axes, axes_label_font=labelfontsize, tickfontsize=ticksize,
                         xlabel_show=True, colorbar_show=True, norm=norm, title="Fiducial EoR Power Spectrum z = 8")
-
-    plot_power_spectrum(u_range, eta, frequency_range, interpolation(u_range, eta),
-                        ratio=True, axes=fiducial_axes[1], axes_label_font=labelfontsize, tickfontsize=ticksize,
-                        xlabel_show=True, colorbar_show=True, norm=norm, title="Fiducial EoR Power Spectrum z = 8")
-
 
     fiducial_figure.tight_layout()
-    pyplot.show()
+    fiducial_figure.savefig(output_path + "Fiducial_EoR_PS_z8.pdf" )
     return
 
 
-
-def read_data():
-    ps_data_path = "/home/ronniyjoseph/Sync/PhD/Projects/beam_perturbations/code/tile_beam_perturbations/" + \
-                   "Data/power_z8_kperp50_kpa50.dat"
+def read_data(ps_data_path):
 
     z_fiducial = 8
     bandwidth_fiducial = 30e6
     central_frequency_fiducial = 1.42e9 / (z_fiducial + 1)
 
-    power_spectrum_fiducial_eor = numpy.fromfile(ps_data_path, dtype=numpy.float32).reshape(50, 50)
-    u_range_fiducial = numpy.linspace(0, 397.15 * numpy.sqrt(2), 51)[1:]
+    power_spectrum_fiducial_eor = numpy.loadtxt(ps_data_path, delimiter=',')
+    u_range_fiducial = numpy.linspace(0, 500, 100)
     frequency_range_fiducial = numpy.linspace(central_frequency_fiducial - bandwidth_fiducial / 2,
-                                              central_frequency_fiducial + bandwidth_fiducial / 2, 100)
+                                              central_frequency_fiducial + bandwidth_fiducial / 2, 251)
 
     dftmatrix, eta_fiducial = dft_matrix(frequency_range_fiducial)
-    hist, eta_fiducial = numpy.histogram(eta_fiducial[:int(len(eta_fiducial) / 2)], bins=50)
+    # hist, eta_fiducial = numpy.histogram(eta_fiducial[:int(len(eta_fiducial) / 2)], bins=251//2)
 
-    return u_range_fiducial, eta_fiducial[:-1], power_spectrum_fiducial_eor
+    return u_range_fiducial, eta_fiducial[:int(len(eta_fiducial) // 2)], power_spectrum_fiducial_eor.T
+
+
+def interpolate(u, eta, u_original, eta_original, ps_data):
+
+    eta_interpolated = numpy.zeros((len(u_original), len(eta)))
+    for i in range(len(u_original)):
+        eta_1d_interp = interp1d(eta_original, ps_data[i, :], kind='cubic')
+        eta_interpolated[i, :] = eta_1d_interp(eta)
+
+    fully_interpolated = numpy.zeros((len(u), len(eta)))
+    for i in range(len(eta)):
+        u_1d_interp = interp1d(u_original, eta_interpolated[:, i], kind='cubic')
+        fully_interpolated[:, i] = u_1d_interp(u)
+
+    return fully_interpolated
+
+
+def fiducial_eor(u, eta, path = "./Data/"
+                 , file = "redshift8.csv"):
+    u_fiducial, eta_fiducial, ps_fiducial = read_data(path + file)
+    ps_interpolated = interpolate(u, eta, u_fiducial, eta_fiducial, ps_fiducial)
+    return ps_interpolated
 
 
 if __name__ == "__main__":
