bb1a2c48b14e95ba02cb104edc0f3ef8bff6aa6e
Ronniy Joseph <ronniy.joseph@icrar.org>
2019-09-04 19:00:22 +0800

    Added ssh option

--- Plot_Calibrated_Error_Comparison.py
+++ Plot_Calibrated_Error_Comparison.py
@@ -7,12 +7,12 @@ from Plot_Fiducial_PS import fiducial_eor
 import argparse
 
 
-def main(labelfontsize = 10, ticksize= 10):
+def main(ssh = False, labelfontsize = 10, ticksize= 10):
     plot_path = "../../Plots/Analytic_Covariance/"
     u_range = numpy.logspace(0, numpy.log10(500), 100)
 
     # 100 frequency channels is fine for now, maybe later do a higher number to push up the k_par range
-    frequency_range = numpy.linspace(135, 165, 251) * 1e6
+    frequency_range = numpy.linspace(135, 165, 101) * 1e6
 
     eta, sky_only_raw, sky_only_cal = residual_ps_error(u_range, frequency_range, residuals='sky')
     eta, sky_and_beam_raw, sky_and_beam_cal = residual_ps_error(u_range, frequency_range, residuals='both')
@@ -22,7 +22,7 @@ def main(labelfontsize = 10, ticksize= 10):
 
     figure, axes = pyplot.subplots(1, 3, figsize = (15, 5))
 
-    ps_norm = colors.LogNorm(vmin = 1e3, vmax = 1e15)
+    ps_norm = colors.LogNorm(vmin = 1e2, vmax = 1e15)
     plot_power_spectrum(u_range, eta, frequency_range, sky_and_beam_cal,
                                   title=r"$\mathbf{C}_{r}$(sky + beam)", axes=axes[0],
                                   axes_label_font= labelfontsize, tickfontsize = ticksize, norm = ps_norm,
@@ -35,7 +35,7 @@ def main(labelfontsize = 10, ticksize= 10):
                         title=r"$\mathbf{C}_{r}$(sky + beam) - $\mathbf{C}_{r}$(sky) ", diff=True,
                         colormap='coolwarm')
 
-    ratio_norm = colors.SymLogNorm(linthresh= 1e1, linscale = 1, vmin = -1e5, vmax = 1e5)
+    ratio_norm = colors.SymLogNorm(linthresh= 1e1, linscale = 1, vmin = -1e1, vmax = 1e5)
     plot_power_spectrum(u_range, eta, frequency_range, difference_cal/fiducial_ps,
                         axes=axes[2], axes_label_font= labelfontsize, tickfontsize = ticksize,
                         norm=ratio_norm, colorbar_show=True, xlabel_show= True,
@@ -43,15 +43,17 @@ def main(labelfontsize = 10, ticksize= 10):
 
     figure.tight_layout()
     figure.savefig(plot_path + "Comparing_Sky_and_Beam_Errors_Post_Calibration.pdf")
-    pyplot.show()
-
+    if not ssh:
+        pyplot.show()
     return
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Plot and compare the sky and beam modelling errors')
-    parser.add_argument('-ssh', type=bool, action='store_true', default=False, help='flag to use when remote plotting')
-    if parser.ssh:
+    parser.add_argument('-ssh', action='store_true', default=False, help='flag to use when remote plotting')
+    args = parser.parse_args()
+
+    if args.ssh:
         matplotlib.use('Agg')
     from matplotlib import pyplot
-    main()
+    main(ssh = args.ssh)
--- Plot_Calibrated_Errors_MWA.py
+++ Plot_Calibrated_Errors_MWA.py
@@ -1,6 +1,5 @@
 import numpy
 import argparse
-
 import matplotlib
 from matplotlib import colors
 
@@ -12,7 +11,7 @@ from plottools import plot_power_spectrum
 from Plot_Fiducial_PS import fiducial_eor
 
 
-def main(labelfontsize=10, ticksize=10):
+def main(ssh = False, labelfontsize=10, ticksize=10):
     plot_path = "../../Plots/Analytic_Covariance/"
     mwa_position_path = "./Data/MWA_Compact_Coordinates.txt"
     mwa_telescope = RadioTelescope(load=True, path=mwa_position_path)
@@ -20,11 +19,11 @@ def main(labelfontsize=10, ticksize=10):
     u_range = numpy.logspace(1, numpy.log10(500), 100)
 
     # 100 frequency channels is fine for now, maybe later do a higher number to push up the k_par range
-    frequency_range = numpy.linspace(135, 165, 251) * 1e6
+    frequency_range = numpy.linspace(135, 165, 101) * 1e6
     weights = compute_weights(u_range, mwa_telescope.baseline_table.u_coordinates,
                               mwa_telescope.baseline_table.v_coordinates)
     eta, sky_only_raw, sky_only_cal = residual_ps_error(u_range, frequency_range, residuals='sky',
-                                                                broken_baselines_weight=0.3, weights = weights) #residual_ps_error(u_range, frequency_range, residuals='sky')
+                                                                broken_baselines_weight=0.3, weights = weights)
     eta, sky_and_beam_raw, sky_and_beam_cal = residual_ps_error(u_range, frequency_range, residuals='both',
                                                                 broken_baselines_weight=0.3, weights= weights)
     fiducial_ps = fiducial_eor(u_range, eta)
@@ -33,7 +32,7 @@ def main(labelfontsize=10, ticksize=10):
 
     figure, axes = pyplot.subplots(1, 3, figsize=(15, 5))
 
-    ps_norm = colors.LogNorm(vmin=1e3, vmax=1e15)
+    ps_norm = colors.LogNorm(vmin=1e2, vmax=1e15)
     plot_power_spectrum(u_range, eta, frequency_range, sky_and_beam_cal,
                         title=r"MWA $\mathbf{C}_{r}$(sky + beam)", axes=axes[0],
                         axes_label_font=labelfontsize, tickfontsize=ticksize, norm=ps_norm,
@@ -46,7 +45,7 @@ def main(labelfontsize=10, ticksize=10):
                         norm=diff_norm, colorbar_show=True, xlabel_show=True,
                         title=r" MWA $\mathbf{C}_{r}$(sky + beam) - $\mathbf{C}_{r}$(sky) ", diff=True, colormap='coolwarm')
 
-    ratio_norm = colors.SymLogNorm(linthresh= 1e1, linscale = 1, vmin = -1e2, vmax = 1e2)
+    ratio_norm = colors.SymLogNorm(linthresh= 1e1, linscale = 1, vmin = -1e1, vmax = 1e5)
 
     plot_power_spectrum(u_range, eta, frequency_range, difference_cal / fiducial_ps,
                         axes=axes[2], axes_label_font=labelfontsize, tickfontsize=ticksize,
@@ -55,15 +54,18 @@ def main(labelfontsize=10, ticksize=10):
 
     figure.tight_layout()
     figure.savefig(plot_path + "Comparing_Sky_and_Beam_Errors_Post_Calibration_MWA.pdf")
-    pyplot.show()
+    if not ssh:
+        pyplot.show()
 
     return
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Plot and compare the sky and beam modelling errors')
-    parser.add_argument('-ssh', type=bool, action='store_true', default=False, help='flag to use when remote plotting')
-    if parser.ssh:
+    parser.add_argument('-ssh',  action='store_true', default=False, help='flag to use when remote plotting')
+    args = parser.parse_args()
+
+    if args.ssh:
         matplotlib.use('Agg')
     from matplotlib import pyplot
-    main()
+    main(ssh = args.ssh)
