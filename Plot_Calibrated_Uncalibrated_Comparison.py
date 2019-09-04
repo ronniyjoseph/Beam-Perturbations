809fac1f1f6993d313c1f01a136a94efbaa1d5b5
Ronniy Joseph <ronniy.joseph@icrar.org>
2019-09-04 18:56:34 +0800

    Added argparse option for ssh

--- Plot_Calibrated_Uncalibrated_Comparison.py
+++ Plot_Calibrated_Uncalibrated_Comparison.py
@@ -1,8 +1,9 @@
 import os
 import numpy
+import argparse
+
 import matplotlib
-matplotlib.use('Agg')
-from matplotlib import pyplot
+
 from matplotlib import colors
 
 from scipy.interpolate import interp2d
@@ -11,27 +12,25 @@ from analytic_covariance import dft_matrix
 from generaltools import from_eta_to_k_par
 from generaltools import from_u_to_k_perp
 from generaltools import from_jansky_to_milikelvin
-from generaltools import colorbar
+from plottools import colorbar
 
-from Plot_Calibrated_Error_Comparison import plot_power_spectrum
+from plottools import plot_power_spectrum
 from Plot_Fiducial_PS import fiducial_eor
 
 
-def main(labelfontsize = 10, ticksize= 10):
+def main(ssh =  False, labelfontsize = 10, ticksize= 10):
     plot_path = "../../Plots/Analytic_Covariance/"
 
     u_range = numpy.logspace(0, numpy.log10(500), 100)
 
     # 100 frequency channels is fine for now, maybe later do a higher number to push up the k_par range
     ###### set to 251 to fill up k from -2
-    frequency_range = numpy.linspace(135, 165, 251) * 1e6
+    frequency_range = numpy.linspace(135, 165, 101) * 1e6
 
     eta, sky_only_raw, sky_only_cal = residual_ps_error(u_range, frequency_range, residuals='sky')
     difference_data = sky_only_cal - sky_only_raw
     fiducial_ps = fiducial_eor(u_range, eta)
 
-    print(difference_data.max())
-    print(fiducial_ps.max())
     figure, axes = pyplot.subplots(1, 3, figsize = (15, 5))
     ps_norm = colors.LogNorm(vmin=1e2, vmax=1e15)
 
@@ -58,10 +57,17 @@ def main(labelfontsize = 10, ticksize= 10):
 
 
     figure.savefig(plot_path + "Comparing_Calibated_and_Uncalibrated.pdf")
-
-    #pyplot.show()
+    if ssh:
+        pyplot.show()
 
     return
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Plot and compare the sky and beam modelling errors')
+    parser.add_argument('-ssh',  action='store_true', default=False, help='flag to use when remote plotting')
+    args = parser.parse_args()
+
+    if args.ssh:
+        matplotlib.use('Agg')
+    from matplotlib import pyplot
+    main(ssh = args.ssh)
